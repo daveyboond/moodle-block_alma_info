f2f2b85aea5b86aa7f42fbbced6e62a2a157ecbb
Chris Fryer <c.j.fryer@lse.ac.uk>
2014-07-04 16:51:15 +0100

    Comments clarifying the inelegance of my XML parsing

--- loans.php
+++ loans.php
@@ -34,13 +34,12 @@ $options = array(
 $alma = new SoapClient($wsdl, $options);
 
 if ($action == 'getloans') {
-
     // Call the Alma web service
     $soapresult = $alma->getUserLoans(array('arg0' => $useridentifier));
     // Parse into SimpleXMLElement
     $searchresults = simplexml_load_string($soapresult->SearchResults);
-
-    // This is hacky, but the SimpleXMLElement class is unruly
+    // This is hacky, but the SimpleXMLElement class is unruly and this 
+    // gives us a stdClass we can actually work with. (TMBABWTDI)
     $output = json_decode(json_encode($searchresults));
     if ($searchresults->errorsExist != 'true') {
         // This is the portion of the response containing patron loans
@@ -48,8 +47,7 @@ if ($action == 'getloans') {
         // which is in the 'xb:' namespace
         $namespaces = $searchresults->getNameSpaces(true);
         $xb = $searchresult->children($namespaces['xb']);
-        // Again, hack this into something more manageable ...
-        //$xb = json_decode(json_encode($xb));
+        // Create an old-fashioned array out of SimpleXML's Iterable filth
         $itemloans = array();
         foreach ($xb->item_loans->item_loan as $itemloan) {
             array_push($itemloans, $itemloan);
